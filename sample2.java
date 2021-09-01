f7de4776febcac93553cff5c26332e69ab9f2314
mahesh <mannemmahesh1996@gmail.com>
2021-09-01 11:25:44 +0530

    added sample2.java

---
+++ sample2.java
@@ -0,0 +1,17 @@
+package com.htc.Demo;
+
+public class Sample {
+	public static void main(String[] args) {
+
+	    int number = 10;
+	    if (number > 0) {
+	      System.out.println("The number is positive.");
+	    }
+
+	    else {
+	      System.out.println("The number is not positive.");
+	    }
+
+	    System.out.println("Statement outside if...else block");
+	  }
+}
